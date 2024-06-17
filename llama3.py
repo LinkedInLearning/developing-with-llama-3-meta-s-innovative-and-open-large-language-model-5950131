5eb73228dd381a91b0b24d98ed2187713e7587ba
Ray Villalobos <ray@planetoftheweb.com>
2024-06-17 17:01:51 -0400

    finished code

--- llama3.py
+++ llama3.py
@@ -4,10 +4,10 @@ with open('groceries.txt', 'r') as file:
     groceries = file.read()
 
 response = ollama.chat(model='llama3', messages=[
-  {
-    'role': 'user',
-    'content': "You are preparing a grocery list, organize them by the most common categories found in a grocery store. " + groceries,
-  },
+    {
+        'role': 'user',
+        'content': "You are preparing a grocery list, organize them by the most common categories found in a grocery store. " + groceries,
+    }    
 ])
 
 with open('sorted.txt', 'w') as file:
