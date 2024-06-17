870ac796bce2d0f9003e31261efa2f19d62284bc
Ray Villalobos <ray@planetoftheweb.com>
2024-06-17 05:32:56 -0400

    --no-edit

--- llama3.py
+++ llama3.py
@@ -1,18 +1,14 @@
 import ollama
 
-# Read the list of groceries from groceries.txt
 with open('groceries.txt', 'r') as file:
     groceries = file.read()
 
-# Pass the list of groceries as the content to the chat model
 response = ollama.chat(model='llama3', messages=[
   {
-    
     'role': 'user',
     'content': "You are preparing a grocery list, organize them by the most common categories found in a grocery store. " + groceries,
   },
 ])
 
-# Write the organized list from the chatbot response to sorted.txt
 with open('sorted.txt', 'w') as file:
     file.write(response['message']['content'])
